e736e66f2c75ddcc15a2521a5605cf84cd7c8c66
devCarlosALexandre <carloalexandresousa@gmail.com>
2023-01-28 13:59:35 -0300

    feat: :sparkles: created component Header
    imported images
    
    styles Header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,12 @@
 import type { AppProps } from 'next/app'
+import '../styles/global.scss'
+import { Header } from '../components/Header'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  )
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html>
+      <Head >
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
+        <link rel="shortcut icon" href="/images/favicon.png" type="image/png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
