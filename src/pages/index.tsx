a683552842752ca0569f8cdce6114a619788c694
devCarlosALexandre <carloalexandresousa@gmail.com>
2023-01-31 11:51:06 -0300

    feat: :sparkles: criado botão de subscribe da home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import styles from './home.module.scss'
+import { SubscribeButton } from '../components/SubscribeButton'
 
 export default function Home() {
   return (
@@ -15,7 +16,9 @@ export default function Home() {
             Get acess to all the publications <br />
             <span>for $9.90 month</span>
           </p>
+          <SubscribeButton />
         </section>
+
 
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
